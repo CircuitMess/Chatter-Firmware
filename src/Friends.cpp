778b048d058969c9631b27e42190749c43ec4179
hrvojekolar <hrvojekolar1998@gmail.com>
2022-01-20 16:00:38 +0100

    Add ListItem to Friends

--- src/Friends.cpp
+++ src/Friends.cpp
@@ -1,6 +1,7 @@
 #include "Friends.h"
 #include "Model/Profile.hpp"
 #include "font.h"
+#include "ListItem.h"
 
 extern std::vector<Profile> friends;
 
@@ -15,32 +16,10 @@ Friends::Friends() : LVScreen(){
 	lv_obj_set_style_bg_img_opa(obj, LV_OPA_100, 0);
 	lv_obj_set_style_bg_img_src(obj, "S:/bg.bin", 0);
 
-	lv_obj_t* addLayout = lv_obj_create(obj);
-	lv_obj_set_height(addLayout, LV_SIZE_CONTENT);
-	lv_obj_set_layout(addLayout,LV_LAYOUT_FLEX);
-	lv_obj_set_flex_flow(addLayout, LV_FLEX_FLOW_ROW);
-	lv_obj_set_flex_align(addLayout, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
-	lv_obj_set_style_pad_gap(addLayout, 3, 0);
-	lv_obj_set_style_border_width(addLayout, 1, 0);
-	lv_obj_set_style_border_color(addLayout, lv_color_white(), 0);
-	lv_obj_set_style_border_opa(addLayout, LV_OPA_100, 0);
-	lv_obj_set_style_pad_all(addLayout, 2, 0);
-	lv_obj_set_width(addLayout, lv_pct(100));
-	lv_obj_set_style_border_width(addLayout, 2, LV_STATE_FOCUSED | LV_PART_MAIN);
-	lv_obj_set_style_pad_all(addLayout, 1, LV_STATE_FOCUSED | LV_PART_MAIN);
-	lv_obj_set_style_bg_color(addLayout, lv_color_black(), LV_STATE_FOCUSED | LV_PART_MAIN);
-	lv_obj_set_style_bg_opa(addLayout, LV_OPA_90, LV_STATE_FOCUSED | LV_PART_MAIN);
+	auto listItem = new ListItem(obj,"Add friend",0,1);
+	lv_obj_set_style_bg_opa(listItem->getLvObj(),0,0);
+	lv_group_add_obj(inputGroup, listItem->getLvObj());
 
-	lv_obj_t* plusImg = lv_img_create(addLayout);
-	lv_img_set_src(plusImg, LV_SYMBOL_PLUS);
-	lv_obj_set_style_text_color(plusImg, lv_color_white(), 0);
-
-	lv_obj_t* addFriendLabel = lv_label_create(addLayout);
-	lv_label_set_text(addFriendLabel,  "Add friend");
-	lv_obj_set_style_text_font(addFriendLabel, &pixelbasic_7, 0);
-	lv_obj_set_style_text_color(addFriendLabel, lv_color_white(), 0);
-
-	lv_group_add_obj(inputGroup, addLayout);
 	for(const Profile& profile : friends){
 		lv_group_add_obj(inputGroup, (new User(obj, profile))->getLvObj());
 	}
@@ -50,4 +29,5 @@ Friends::Friends() : LVScreen(){
 		lv_obj_invalidate(lv_obj_get_parent(focused));
 		lv_obj_scroll_to_view(focused, LV_ANIM_ON);
 	});
+
 }
